93ad283b903fcb1e8633e1f8c6981e7781b70336
Александр <adsl41c@gmail.com>
2018-08-18 04:08:27 +0000

    v0.6.2 add trim function to validate username

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,7 +13,7 @@ router.get('/', BlogController.index.blog)
 // User Registration
 router.get('/register', UserController.register.Get)
 router.post('/register', [
-  check('username').isLength({ min: 1, max: 128 }).withMessage('Name is required.'),
+  check('username').isLength({ min: 1, max: 128 }).withMessage('Name is required.').trim(),
   check('email').isLength({ min: 1, max: 128 }).withMessage('Email is required.').isEmail().withMessage('Please provide a valid email address').normalizeEmail(),
   check('password').isLength({ min: 6 }).withMessage('Invalid password. Password must be at least minimum 6'),
   check('password2').exists().custom((value, { req }) => value === req.body.password).withMessage('"Confirm your Password" field must have the same value as the Password field')
@@ -22,7 +22,7 @@ router.post('/register', [
 // User Login
 router.get('/login', UserController.login.Get)
 router.post('/login', [
-  check('username').isLength({min: 1, max: 128}).withMessage('Name or Email is required.'),
+  check('username').isLength({min: 1, max: 128}).withMessage('Name or Email is required.').trim(),
   check('password').isLength({ min: 6 }).withMessage('Invalid password')
 ], validation.user.login, UserController.login.Post)
 router.get('/logout', UserController.logout)
